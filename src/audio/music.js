9b0f8c3e10f5085223e22a6e7a91c3eda7a72b45
Tiago Correia <tiago.f.a.correia@gmail.com>
2025-09-06 01:38:36 +0100

    Different tune

--- src/audio/music.js
+++ src/audio/music.js
@@ -1,111 +1,73 @@
 import { Music, Note, Track } from '../utils/audio-utils';
 
-const accompaniamentTracks = [
-    new Track([
-        Note.new("g#", 3, 5), null, Note.new("g#", 3, 5), null,
-        Note.new("a#", 3, 5), null, Note.new("a#", 3, 5), null,
-        Note.new("b", 3, 5), null, Note.new("b", 3, 5), null,
-        Note.new("g#", 3, 5), null, Note.new("g#", 3, 5), null,
-        Note.new("f#", 3, 5), null, Note.new("f#", 3, 5), null,
-        Note.new("e", 3, 5), null, Note.new("e", 3, 5), null,
-        Note.new("c#", 3, 5), null, Note.new("c#", 3, 5), null,
-        Note.new("d#", 3, 5), null, Note.new("d#", 3, 5), null,
-        Note.new("f#", 3, 5), null, Note.new("f#", 3, 5), null,
-        Note.new("g#", 3, 5), null, Note.new("g#", 3, 5), null,
-        Note.new("a#", 3, 5), null, Note.new("a#", 3, 5), null,
-        Note.new("b", 3, 5), null, Note.new("b", 3, 5), null,
-        Note.new("g#", 3, 5), null, Note.new("f#", 3, 5), null,
-        Note.new("e", 3, 5), null, Note.new("c#", 3, 5), null
-    ], "triangle", 2, 0.02, 11),
-    new Track([
-        Note.new("c", 2, 4), null, null, null,
-        Note.new("f#", 2, 4), null, null, null,
-        Note.new("c", 2, 4), null, Note.new("b", 2, 4), null,
-        Note.new("f#", 2, 4), null, null, null,
+const mysteriousMelody = new Track([
+  Note.new("c", 5, 4), null, Note.new("c#", 5, 4), Note.new("d#", 5, 4),
+  null, Note.new("f#", 5, 4), Note.new("e", 5, 4), null,
+  Note.new("g#", 5, 4), Note.new("a#", 5, 4), null, Note.new("b", 5, 4),
+  Note.new("a#", 5, 4), Note.new("g#", 5, 4), Note.new("f#", 5, 4), null,
+  Note.new("c", 6, 4), null, Note.new("c#", 6, 4), Note.new("d#", 6, 4),
+  null, Note.new("f#", 6, 4), Note.new("e", 6, 4), null,
+  Note.new("g#", 6, 4), Note.new("a#", 6, 4), null, Note.new("b", 6, 4),
+  Note.new("a#", 6, 4), Note.new("g#", 6, 4), Note.new("f#", 6, 4), null,
+  Note.new("f#", 5, 4), null, Note.new("c", 5, 4), null,
+  Note.new("f#", 5, 4), null, Note.new("c#", 5, 4), Note.new("d#", 5, 4),
+  null, Note.new("e", 5, 4), null, Note.new("g#", 5, 4),
+  Note.new("a#", 5, 4), null, Note.new("b", 5, 4), null,
+  Note.new("a#", 5, 4), Note.new("g#", 5, 4), null, Note.new("f#", 5, 4),
+  Note.new("e", 5, 4), null, Note.new("d#", 5, 4), Note.new("c#", 5, 4),
+  null, Note.new("c", 5, 4), null, Note.new("b", 4, 4),
+  Note.new("c", 5, 4), null, Note.new("f#", 5, 4), null
+], "triangle", 2, 0.012, 20);
 
-        Note.new("c", 2, 4), null, null, null,
-        Note.new("f#", 2, 4), null, null, null,
-        Note.new("c", 2, 4), null, Note.new("a#", 2, 4), null,
-        Note.new("f#", 2, 4), null, null, null,
+const mysteriousOstinato = new Track([
+  Note.new("f#", 3, 5), null, Note.new("c", 3, 5), null,
+  Note.new("g#", 2, 5), null, Note.new("c#", 3, 5), null,
 
-        Note.new("c", 2, 4), null, null, null,
-        Note.new("f#", 2, 4), null, null, null,
-        Note.new("c", 2, 4), null, Note.new("b", 2, 4), null,
-        Note.new("f#", 2, 4), null, null, null,
+  Note.new("f#", 3, 5), null, Note.new("c", 3, 5), null,
+  Note.new("g#", 2, 5), null, Note.new("c#", 3, 5), null,
 
-        Note.new("c", 2, 4), null, null, null,
-        Note.new("f#", 2, 4), null, null, null,
-        Note.new("c", 2, 4), null, Note.new("a#", 2, 4), null,
-        Note.new("f#", 2, 4), null, null, null
-    ], "square", 2, 0.005, 11)
-];
+  Note.new("f#", 3, 5), null, Note.new("c", 3, 5), null,
+  Note.new("g#", 2, 5), null, Note.new("c#", 3, 5), null,
 
-const melodyTrack = new Track([
-    Note.new("c", 5, 4),
-    null,
-    Note.new("c#", 5, 4),
-    Note.new("d#", 5, 4),
-    null,
-    Note.new("e", 5, 4),
-    Note.new("f#", 5, 4),
-    null,
-    Note.new("g#", 5, 4),
-    Note.new("a#", 5, 4),
-    null,
-    Note.new("b", 5, 4),
-    Note.new("a#", 5, 4),
-    null,
-    Note.new("g#", 5, 4),
-    Note.new("f#", 5, 4),
-    Note.new("e", 5, 4),
-    null,
-    Note.new("d#", 5, 4),
-    Note.new("c#", 5, 4),
-    null,
-    Note.new("c", 5, 4),
-    Note.new("c#", 5, 4),
-    null,
-    Note.new("d#", 5, 4),
-    Note.new("e", 5, 4),
-    null,
-    Note.new("f#", 5, 4),
-    Note.new("g#", 5, 4),
-    null,
-    Note.new("a#", 5, 4),
-    Note.new("b", 5, 4),
-    null,
-    Note.new("a#", 5, 4),
-    Note.new("g#", 5, 4),
-    Note.new("f#", 5, 4),
-    null,
-    Note.new("e", 5, 4),
-    Note.new("d#", 5, 4),
-    null,
-    Note.new("c#", 5, 4),
-    Note.new("c", 5, 4),
-    null,
-    Note.new("c#", 5, 4),
-    Note.new("d#", 5, 4),
-    Note.new("e", 5, 4),
-    null,
-    Note.new("f#", 5, 4),
-    Note.new("g#", 5, 4),
-    Note.new("a#", 5, 4),
-    null,
-    Note.new("b", 5, 4),
-    Note.new("a#", 5, 4),
-    Note.new("g#", 5, 4),
-    null,
-    Note.new("f#", 5, 4),
-    Note.new("e", 5, 4),
-    Note.new("d#", 5, 4),
-    null,
-    Note.new("c#", 5, 4),
-    Note.new("c", 5, 4)
-], "triangle", 2, 0.01, 33);
+  Note.new("f#", 3, 5), null, Note.new("c", 3, 5), null,
+  Note.new("g#", 2, 5), null, Note.new("c#", 3, 5), null,
+
+  Note.new("f#", 3, 5), null, Note.new("c", 3, 5), null,
+  Note.new("g#", 2, 5), null, Note.new("c#", 3, 5), null,
+
+  Note.new("f#", 3, 5), null, Note.new("c", 3, 5), null,
+  Note.new("g#", 2, 5), null, Note.new("c#", 3, 5), null,
+
+  Note.new("f#", 3, 5), null, Note.new("c", 3, 5), null,
+  Note.new("g#", 2, 5), null, Note.new("c#", 3, 5), null,
+
+  Note.new("f#", 3, 5), null, Note.new("c", 3, 5), null,
+  Note.new("g#", 2, 5), null, Note.new("c#", 3, 5), null
+], "triangle", 2, 0.02, 11);
+
+const mysteriousBeat = new Track([
+  Note.new("c", 2, 4), Note.new("b", 3, 4), Note.new("f#", 2, 4), Note.new("a#", 3, 4),
+  Note.new("c", 2, 4), Note.new("b", 3, 4), Note.new("f#", 2, 4), Note.new("a#", 3, 4),
+  Note.new("c", 2, 4), Note.new("b", 3, 4), Note.new("f#", 2, 4), Note.new("a#", 3, 4),
+  Note.new("c", 2, 4), Note.new("b", 3, 4), Note.new("f#", 2, 4), Note.new("a#", 3, 4),
+
+  Note.new("c", 2, 4), Note.new("b", 3, 4), Note.new("f#", 2, 4), Note.new("a#", 3, 4),
+  Note.new("c", 2, 4), Note.new("b", 3, 4), Note.new("f#", 2, 4), Note.new("a#", 3, 4),
+  Note.new("c", 2, 4), Note.new("b", 3, 4), Note.new("f#", 2, 4), Note.new("a#", 3, 4),
+  Note.new("c", 2, 4), Note.new("b", 3, 4), Note.new("f#", 2, 4), Note.new("a#", 3, 4),
+
+  Note.new("c", 2, 4), Note.new("b", 3, 4), Note.new("f#", 2, 4), Note.new("a#", 3, 4),
+  Note.new("c", 2, 4), Note.new("b", 3, 4), Note.new("f#", 2, 4), Note.new("a#", 3, 4),
+  Note.new("c", 2, 4), Note.new("b", 3, 4), Note.new("f#", 2, 4), Note.new("a#", 3, 4),
+  Note.new("c", 2, 4), Note.new("b", 3, 4), Note.new("f#", 2, 4), Note.new("a#", 3, 4),
+
+  Note.new("c", 2, 4), Note.new("b", 3, 4), Note.new("f#", 2, 4), Note.new("a#", 3, 4),
+  Note.new("c", 2, 4), Note.new("b", 3, 4), Note.new("f#", 2, 4), Note.new("a#", 3, 4),
+  Note.new("c", 2, 4), Note.new("b", 3, 4), Note.new("f#", 2, 4), Note.new("a#", 3, 4),
+  Note.new("c", 2, 4), Note.new("b", 3, 4), Note.new("f#", 2, 4), Note.new("a#", 3, 4)
+], "square", 2, 0.004, 9);
 
 export const music = new Music(
-    [melodyTrack, ...accompaniamentTracks],
-    260
+  [mysteriousMelody, mysteriousOstinato, mysteriousBeat],
+  240
 );
-
